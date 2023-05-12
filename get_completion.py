8b4425d9063a5a2d323fba4d380e3b806cdb0d17
wintenboy <baut3993@gmail.com>
2023-05-12 15:38:51 +0900

    캡스톤디자인

--- get_completion.py
+++ get_completion.py
@@ -1,10 +1,10 @@
 import openai
 
-def get_completion(prompt, model="gpt-3.5-turbo"): # Andrew mentioned that the prompt/ completion paradigm is preferable for this class
+def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
+        temperature=0,
     )
     return response.choices[0].message["content"]
